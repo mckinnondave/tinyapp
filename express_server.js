8ab6c4de110de54ac96333a51e4111d7bb4aa9b4
Dave McKinnon <dave.w.mckinnon@gmail.com>
2022-03-22 16:05:11 +0000

    added server side ability to delete shortURL. redirects back to same page.

--- express_server.js
+++ express_server.js
@@ -28,6 +28,11 @@ app.post("/urls", (req, res) => {
   res.redirect(`/urls/${randomKey}`);
 });
 
+app.post("/urls/:shortURL/delete", (req, res) => {
+  delete urlDatabase[req.params.shortURL]
+  res.redirect("/urls/")
+})
+
 app.get("/u/:shortURL", (req, res) => {
   const longURL = urlDatabase[req.params.shortURL]
   res.redirect(longURL);
