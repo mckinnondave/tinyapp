6c2940842d2903d76533d81989844bd6eb7b1d8e
Dave McKinnon <dave.w.mckinnon@gmail.com>
2022-03-22 03:25:09 +0000

    changed order of /urls with /urls/new

--- express_server.js
+++ express_server.js
@@ -37,13 +37,13 @@ app.get("/", (req, res) => {
   res.send("Hello!");
 });
 
-app.get("/urls/new", (req, res) => {
-  res.render("urls_new")
-})
-
 app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase}
   res.render("urls_index", templateVars)
+})
+
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new")
 })
 
 app.get("/urls/:shortURL", (req, res) => {
