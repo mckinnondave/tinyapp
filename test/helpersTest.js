e480b781a651c7d9b98c1315558aa6bdf8992334
Dave McKinnon <dave.w.mckinnon@gmail.com>
2022-03-24 21:12:02 +0000

    eslint on helpers and corrected strictEqual error

--- test/helpersTest.js
+++ test/helpersTest.js
@@ -4,27 +4,27 @@ const { getUserByEmail } = require('../helpers.js');
 
 const testUsers = {
   "userRandomID": {
-    id: "userRandomID", 
-    email: "user@example.com", 
+    id: "userRandomID",
+    email: "user@example.com",
     password: "purple-monkey-dinosaur"
   },
   "user2RandomID": {
-    id: "user2RandomID", 
-    email: "user2@example.com", 
+    id: "user2RandomID",
+    email: "user2@example.com",
     password: "dishwasher-funk"
   }
 };
 
 describe('getUserByEmail', function() {
   it('should return a user with valid email', function() {
-    const user = getUserByEmail(testUsers, "user@example.com")
+    const user = getUserByEmail(testUsers, "user@example.com");
     const expectedUserID = "userRandomID";
-    assert.strictEqual(user.id, expectedUserID)
+    assert.strictEqual(user.id, expectedUserID);
   });
 
   it("should return undefined if email is not in users database", function() {
-    const user = getUserByEmail(testUsers, "user3@example.com");
+    const user = getUserByEmail(testUsers, "newUser@example.com");
     const expectedUserID = undefined;
-    assert.strictEqual(user.id, undefined);
-  })
+    assert.strictEqual(user.id, expectedUserID);
+  });
 });
